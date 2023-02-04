59c7fabf435993b969eb30f710bae16298b95e9f
Anish Patil <123080347+Anish-1504@users.noreply.github.com>
2023-02-04 17:17:57 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!\n")
+
+height = int(input("What is your height in cm? "))
+if height==120:
+ print("can ride roller coster")
+else: 
+ print("u can not ride the roller coster")
+  
